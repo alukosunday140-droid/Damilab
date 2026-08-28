62c9755ed22c58727fa01041c1984656d481ffc2
alukosunday140-droid <alukosunday140@gmail.com>
2026-08-28 09:29:51 +0100

    chore: improve development server configuration

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
+import os
+
 from app import app
 
 
 if __name__ == "__main__":
-    app.run()
+    debug = os.getenv("FLASK_DEBUG", "0") == "1"
+    app.run(host="127.0.0.1", port=5000, debug=debug)
