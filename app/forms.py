e9c3254d566a9803f8df5c722fd09a1996efc0a3
alukosunday140-droid <alukosunday140@gmail.com>
2026-08-29 03:08:27 +0100

    feat: add validated contact form

---
+++ app/forms.py
@@ -0,0 +1,22 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Email, Length
+
+
+class ContactForm(FlaskForm):
+    name = StringField(
+        "Name",
+        validators=[DataRequired(), Length(min=2, max=100)],
+    )
+
+    email = StringField(
+        "Email",
+        validators=[DataRequired(), Email()],
+    )
+
+    message = TextAreaField(
+        "Message",
+        validators=[DataRequired(), Length(min=10, max=1000)],
+    )
+
+    submit = SubmitField("Send Message")
